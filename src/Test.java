009457346dc50e71ca2ecb940c7e84ec127c092e
moring <moring_jiang@163.com>
2020-07-12 17:11:02 +0800

    second test

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Author:   jiang
+ * Date:     2020/7/12 17:09
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("this is second test about git");
+    }
+}
