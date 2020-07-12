9d562e911ceb3af691e38aa6e48dc4f403ed052a
moring <moring_jiang@163.com>
2020-07-12 16:48:12 +0800

    first commit

---
+++ src/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * Author:   jiang
+ * Date:     2020/7/12 16:43
+ */
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
