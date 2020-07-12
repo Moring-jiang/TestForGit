908d52288846e7ae2c884b30483bc1da774ea892
moring <moring_jiang@163.com>
2020-07-12 17:28:00 +0800

    second commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Author:   jiang
+ * Date:     2020/7/12 17:25
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("this is second commit by use git");
+    }
+}
